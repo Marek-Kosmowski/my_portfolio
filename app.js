efab3ecbe37938b79b9502db001d9ee495c84b91
Marek-Kosmowski <kosmowski.marek@gmail.com>
2022-03-28 19:28:02 +0200

    adding starting files

---
+++ app.js
@@ -0,0 +1,26 @@
+// function myFunction(arr) {
+
+//     if(arr.every())
+
+//     // arr.every(v => v === arr[0])
+// }
+
+
+function myFunction(number) {
+    if (number % 2 === 0) {
+        return true
+    } else {
+        return false
+    }
+}
+// console.log('test')
+
+
+// const allEqual = arr => arr.every(v => v === arr[0])
+
+// const allEqual = function (arr) {
+//     arr.every(el => el === arr[0])
+// }
+// // allEqual([1, 1, 1, 1])
+
+// // console.log('test')
